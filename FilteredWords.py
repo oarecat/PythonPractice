3aaaf26112fc0db552874bee7e43bb8237b462b3
韩常铭 <han.changming@qq.com>
2016-10-12 10:57:25 +0800

    完成题目10、11
    * 2016年10月12日：完成第10题，使用Python生成类似于下图中的字母验证码图片-VerificationCode
    *
    2016年10月12日：完成第11题，敏感词文本文件filtered_words.txt,里面的内容为以下内容,当用户输入敏感词语时,则打印出Freedom,否则打印出Human
    Rights-FilteredWords

---
+++ FilteredWords.py
@@ -0,0 +1,13 @@
+#第0011题:敏感词文本文件filtered_words.txt,里面的内容为以下内容,当用户输入敏感词语时,则打印出Freedom,否则打印出Human Rights.
+
+userInput = input("请输入文字：")
+
+#增加的encoding = "utf-8"表示读取文件时按照UTF-8的编码方式
+filterWords = open("C:\\GitHub\\PythonPractice\\filtered_words.txt" , "r" , encoding="utf-8").read().split(",")
+
+print (filterWords)
+
+if userInput in filterWords:
+    print ("Freedom")
+else:
+    print ("Human Rights")
